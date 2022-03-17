a38c10d0eb68ab3f2e6c8f65b21d735ce170e711
ValentinaRippe <lauravalentina200102@gmail.com>
2022-03-17 08:58:41 -0500

    Style finish

--- src/scenes/Resume/Resume.jsx
+++ src/scenes/Resume/Resume.jsx
@@ -1,39 +1,15 @@
+import { useState } from 'react';
 import s from './Resume.module.scss';
 import { ReactComponent as FilesIcon } from '../../assets/resume-files.svg';
 import { ReactComponent as DownloadIcon } from '../../assets/download.svg';
-import { useEffect, useRef, useState } from 'react';
 import BaseLayout from '../../layouts/BaseLayout/BaseLayout';
-import LinerProgress from '../../components/UIElements/LinerProgress/LinerProgress';
 import Button from '../../components/UIElements/Button/Button';
-//react-pdf
-import { Document, Page, pdfjs } from 'react-pdf';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const resumeLink =
-  'https://raw.githubusercontent.com/vsnaichuk/vsnaichuk/master/CV.pdf';
+'http://drive.google.com/uc?export=view&id=1TBPJXgixBxDkw4FIi370G-T7zDWtAh-X';
 
 const Resume = () => {
   const [name, setName] = useState("");
-  const pdfWrapper = useRef(null);
-  const [pdfPageWidth, setPdfPageWidth] = useState(null);
-  useEffect(() => {
-    setPdfPageWidth(
-      pdfWrapper.current?.getBoundingClientRect().width || null,
-    );
-  }, []);
-
-  const removeTextLayerOffset = () => {
-    const textLayers = document.querySelectorAll(
-      '.react-pdf__Page__textContent',
-    );
-    textLayers.forEach((layer) => {
-      const { style } = layer;
-      style.top = '0';
-      style.left = '0';
-      style.transform = '';
-    });
-  };
   //Form submit
 
   function handleSubmit(event) {
@@ -47,7 +23,7 @@ const Resume = () => {
       <div className={s.content}>
         <div className={s.header}>
           <h1 className={s.title}>
-            My <br /> Resume
+            Mi <br /> Resumen
           </h1>
 
           <div className={s.filesImg}>
@@ -65,32 +41,11 @@ const Resume = () => {
           <span className={s.downloadText}> download resume</span>
           <span className={s.filename}>.pdf</span>
         </Button>
-
-        <div className={s.pdfWrapper} ref={pdfWrapper}>
-          <Document
-            loading={<LinerProgress />}
-            file={{
-              url: resumeLink,
-            }}
-          >
-            <Page
-              onLoadSuccess={removeTextLayerOffset}
-              loading={<LinerProgress />}
-              width={pdfPageWidth}
-              pageNumber={1}
-            />
-          </Document>
-        </div>
       </div>
-      <div className="u-wrapper">
-        <div className="contact-content">
-          <form onSubmit={handleSubmit} className="form">
-            <img
-              className="logocard"
-              src="img/logofooter.png"
-              alt=""
-            />
+        <div className={s.contact}>
+          <form className={s.form} onSubmit={handleSubmit} > 
             <input
+            className={s.inputs}
               name="name"
               required
               type="text"
@@ -99,6 +54,7 @@ const Resume = () => {
               placeholder='Name'
             />
             <input
+            className={s.inputs}
               name="email"
               type="text"
               id="email"
@@ -113,18 +69,16 @@ const Resume = () => {
               rows="10"
               placeholder="Message"
             ></textarea>
-            <button type="submit" className="btnSubmit">
+            <button type="submit" className={s.btnSubmit}>
               Submit
             </button>
             <a
               href="mailto:lauravalentina200102@gmail.com"
               id="emailSubmit"
             >
-              mail
             </a>
           </form>
         </div>
-      </div>
     </BaseLayout>
   );
 };
